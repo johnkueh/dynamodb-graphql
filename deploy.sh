0b18cbd14f36dc68181bca2a14c743bc3730420f
John Kueh <yoongfook@gmail.com>
2019-08-10 10:17:28 +1000

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-npm install -g serverless
+npm install --silent --no-progress -g yarn
+yarn global add serverless serverless-webpack --silent --no-progress
 serverless deploy --stage $env --package \   $CODEBUILD_SRC_DIR/target/$env -v -r us-east-1
