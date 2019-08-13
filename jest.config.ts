0aff3f3c1c04546d5dcf0d425ec5cb8e11b22ba1
John Kueh <yoongfook@gmail.com>
2019-08-14 09:55:58 +1000

    Remove unused files

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
   clearMocks: true,
-  setupFiles: ["<rootDir>/test/support/jest-setup.js"],
+  setupFiles: ["<rootDir>/test/support/jest-setup.ts"],
   preset: "ts-jest",
   testEnvironment: "node"
 };
